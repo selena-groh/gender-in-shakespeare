aa1656107d8aab0a1ab32ab5c71f251a3e97c205
Selena Groh <selena.groh@gmail.com>
2018-11-27 19:20:48 -0500

    Add colors, tooltip, labels

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 (function () {
 
-const mapGenreToColor = {'Comedy': '#4CB944', 'History': '#246EB9', 'Tragedy': '#F06543'};
-const genres = [{'genre': 'Comedy', 'color': '#4CB944'}, {'genre': 'History', 'color': '#246EB9'}, {'genre': 'Tragedy', 'color': '#F06543'}];
+const mapGenreToColor = {'Comedy': '#4CB944', 'History': '#AF4DA4', 'Tragedy': '#F47835'};
+const genres = [{'genre': 'Comedy', 'color': '#4CB944'}, {'genre': 'History', 'color': '#AF4DA4'}, {'genre': 'Tragedy', 'color': '#F47835'}];
+const colorM = '#08B2E3',
+  colorW = '#EC3E5B';
 
 d3.json('data/shakes-plays-chars.json', function(error, data) {
     if (error) throw error;
+
+    makeTimeline();
 
     function makeTimeline() {
       const circleRadius = 4;
@@ -12,7 +16,7 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
 
       const svg = d3.select('#timeline svg'),
           legendMargin = {top: 30, right: 0, bottom: 0, left: 25},
-          margin = {top: 50, right: 30, bottom: 30, left: 25},
+          margin = {top: 50, right: 30, bottom: 20, left: 25},
           width = +svg.attr('width') - margin.left - margin.right,
           height = +svg.attr('height') - margin.top - margin.bottom;
 
@@ -101,59 +105,131 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
       makeLine(svg);
     }
 
-    makeTimeline();
+    makePlays();
 
     function makePlays() {
-      const svg = d3.select('#plays svg'),
-          margin = {top: 20, right: 20, bottom: 30, left: 50},
-          width = +svg.attr('width'),
-          height = +svg.attr('height'),
-          domainwidth = width - margin.left - margin.right,
-          domainheight = height - margin.top - margin.bottom;
+      const playPrefix = 'play-';
 
-      var x = d3.scaleLinear()
-          .domain([-100, 100])
-          .range(padExtent([0, domainwidth]));
-      var y = d3.scaleLinear()
-          .domain([-100, 100])
-          .range(padExtent([domainheight, 0]));
-
-      var g = svg.append("g")
-          .attr("transform", "translate(" + margin.top + "," + margin.top + ")");
-
-      g.append("rect")
-          .attr("width", width - margin.left - margin.right)
-          .attr("height", height - margin.top - margin.bottom)
-          .attr("fill", "none");
+      const plays = d3.select('#plays'),
+        svg = plays.select('svg'),
+        margin = {top: 40, right: 30, bottom: 30, left: 30},
+        width = +svg.attr('width'),
+        height = +svg.attr('height'),
+        domainwidth = width - margin.left - margin.right,
+        domainheight = height - margin.top - margin.bottom;
 
       data.forEach(function(d) {
-          d.consequence = +d.consequence;
-          d.value = +d.value;
-          d.deltaSum = percentDifference(d.sumW, d.sumM);
-          d.deltaAvg = percentDifference(d.avgW, d.avgM);
+        d.diffSum = percentDifference(d.sumW, d.sumM);
+        d.diffAvg = percentDifference(d.avgW, d.avgM);
       });
 
-      g.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + y(0) + ")")
-          .call(d3.axisBottom(x).ticks(10).tickFormat(function(d) { return d === 0 ? '' : Math.abs(d) + '%'; }));
+      var tooltip = plays.append('div')
+        .attr('class', 'tooltip');
 
-      g.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(" + x(0) + ", 0)")
-          .call(d3.axisRight(y).ticks(10).tickFormat(function(d) { return d === 0 ? '' : Math.abs(d) + '%'; }));
+      var g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-      g.selectAll("circle")
+      g.append('rect')
+        .attr('width', width - margin.left - margin.right)
+        .attr('height', height - margin.top - margin.bottom)
+        .attr('fill', 'none');
+
+      var x = d3.scaleLinear()
+        .domain([-100, 100])
+        .range(padExtent([0, domainwidth]));
+      var y = d3.scaleLinear()
+        .domain([-100, 100])
+        .range(padExtent([domainheight, 0]));
+
+      makeAxes(g);
+      function makeAxes(parent) {
+        makeYAxis(parent);
+        makeXAxis(parent);
+
+        function makeXAxis(parent) {
+          parent.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + y(0) + ')')
+            .call(d3.axisBottom(x).ticks(10).tickFormat(function(d) { return d === 0 ? '' : Math.abs(d) + '%'; }));
+
+          parent.append('text')
+            .attr('class', 'axis-label')
+            .attr('transform',
+                  'translate(' + x(-100) + ' ,' +
+                                 y(5) + ')')
+            .style('text-anchor', 'Left')
+            .text('Average Word Count Difference');
+        }
+
+        function makeYAxis(parent) {
+          parent.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate(' + x(0) + ', 0)')
+            .call(d3.axisRight(y).ticks(10).tickFormat(function(d) { return d === 0 ? '' : Math.abs(d) + '%'; }));
+
+          parent.append('text')
+            .attr('class', 'axis-label')
+            .attr('transform',
+                  'translate(' + x(0) + ' ,' +
+                                 y(105) + ')')
+            .style('text-anchor', 'middle')
+            .text('Total Word Count Difference');
+        }
+
+        parent.selectAll('.tick text')
+          .attr('fill', function(d) { return d < 0 ? colorM : colorW; });
+
+        parent.selectAll('.tick line')
+          .attr('stroke', function(d) { if (d === 0) { return 'none'; } return d < 0 ? colorM : colorW; });
+      }
+
+      g.selectAll('circle')
           .data(data)
-        .enter().append("circle")
-          .attr("class", "dot")
-          .attr('id', function(d) { return d.id; })
-          .attr('deltaAvg', function(d) { return d.deltaAvg; })
-          .attr('deltaSum', function(d) { return d.deltaSum; })
-          .attr("r", 4)
-          .attr("cx", function(d) { return x(d.deltaAvg); })
-          .attr("cy", function(d) { return y(d.deltaSum); })
-          .attr('fill', function(d) { return mapGenreToColor[d.genre]; });
+        .enter().append('circle')
+          .attr('class', 'dot')
+          .attr('id', function(d) { return playPrefix + d.id; })
+          .attr('diffAvg', function(d) { return d.diffAvg; })
+          .attr('diffSum', function(d) { return d.diffSum; })
+          .attr('r', 6)
+          .attr('cx', function(d) { return x(d.diffAvg); })
+          .attr('cy', function(d) { return y(d.diffSum); })
+          .style('stroke', 'white')
+          .style('stroke-width', '0.5px')
+          .style('fill', function(d) { return mapGenreToColor[d.genre]; })
+          .style('cursor', 'pointer')
+          .on('mouseover', handleMouseover)
+          .on('mouseout', handleMouseout);
+
+      function handleMouseover(d) {
+        d3.select('#' + playPrefix + d.id)
+          .transition()
+            .duration(100)
+            .style('stroke', 'black')
+            .style('stroke-width', '1px');
+
+        tooltip.style('display', 'block');
+        tooltip.transition()
+          .duration(100)
+          .style('opacity', 0.8);
+        tooltip.html(tooltipText(d))
+          .style('left', (d3.event.pageX - 115) + 'px')
+          .style('top', (d3.event.pageY - 60) + 'px');
+      }
+
+      function handleMouseout(d) {
+        d3.select('#' + playPrefix + d.id)
+          .transition()
+            .duration(200)
+            .style('stroke', 'white')
+            .style('stroke-width', '0.5px');
+
+        tooltip.transition()
+          .duration(200)
+          .style('opacity', 0)
+          .on('end', function() {
+            tooltip.style('display', 'none');
+          });
+      }
 
       function padExtent(e, p) {
           if (p === undefined) p = 1;
@@ -163,9 +239,31 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
       function percentDifference(a, b) {
         return (a - b) / (a + b) * 100;
       }
-    }
 
-    makePlays();
+      function tooltipText(d) {
+        let text = '<span class="title">' + d.title + '</span><span class="avgVsSum">';
+
+        text += statSummary(d.diffAvg, 'on average');
+        text += '<br/>';
+        text += statSummary(d.diffSum, 'total');
+
+        text += '</span>';
+
+        return text;
+      }
+
+      function statSummary(n, w) {
+        const greaterGender = n < 0 ? 'male' : 'female';
+        let text = n < 0 ? 'Males' : 'Females';
+        text += ' have <span class="' + greaterGender + '">' + printPercent(n) + ' more</span> words ' + w + ' than ';
+        text += n < 0 ? 'females' : 'males';
+        return text;
+      }
+
+      function printPercent(a) {
+        return Math.round(Math.abs(a)) + '%';
+      }
+    }
 
 });
 
