11061e38d44af0fd9e7cd9315b9eef71c869f2ae
Emily Davis <emilyjdavis94@gmail.com>
2018-11-27 20:21:47 -0500

    made barchart horizontal and accurate

--- js/scripts.js
+++ js/scripts.js
@@ -278,7 +278,7 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
 
 	var charset = myplay.characters;
 	charset = charset.sort(function (a, b) {
-		return d3.ascending(a.wc, b.wc); 
+		return d3.descending(a.wc, b.wc); 
 	 });
 
 	var numchars = myplay.characters.length;
@@ -290,13 +290,13 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
 
 	var gap = 0.2;
 
-	let xScaler = d3.scaleBand()
-	        .rangeRound([0, charwidth])
+	let yScaler = d3.scaleBand()
+	        .rangeRound([0, charheight])
 		.padding(gap)
 		.domain(charset.map(function(d){ return d.who;
 		 }));
-	let yScaler = d3.scaleLinear()
-		.rangeRound([charheight,0])
+	let xScaler = d3.scaleLinear()
+		.rangeRound([0, charwidth])
 		.domain([0, d3.max(charset.map(function(d){ return d.wc;
 		 }))]);
 
@@ -307,17 +307,23 @@ d3.json('data/shakes-plays-chars.json', function(error, data) {
 	    .data(charset)
 	    .enter()
 	    .append("rect")
-	    .attr("x", function(d) {
-	        return xScaler(d.who);
-	     })
+	    //.attr("x", function(d) {
+	    //    return yScaler(d.who);
+	    // })
 	    .attr("y", function(d) {
-	        return yScaler(+d.wc);
+	        return yScaler(d.who);
   	     })
-	    .attr("width", xScaler.bandwidth())
-	    .attr("height", function(d) {
-	        return charheight - yScaler(+d.wc);
+	    .attr("height", yScaler.bandwidth())
+	    .attr("width", function(d) {
+	        return xScaler(+d.wc);
 	     })
-	    .attr("fill", "teal");
+	    .attr("fill", function(d) {
+		if (d.gender == 'male') {
+		    return colorM;
+ 		} else {
+		    return colorW;
+		}
+	      });
 
 	var x_axis = barChart.append("g")
 	    		.attr("transform", "translate(0, " + charheight + ")")
